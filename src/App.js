db5cf3149ce8227b6ab6cd92064237260867f3ca
Your Name <you@example.com>
2023-01-14 15:16:20 -0800

    cart toggle working

--- src/App.js
+++ src/App.js
@@ -5,13 +5,8 @@ import Navigation from './routes/navigation/navigation.component';
 import './App.css';
 import {Routes, Route, Outlet} from 'react-router-dom';
 import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
 
-
-const Shop = () => {
-  return (
-    <h1>I am the shop page</h1>
-  )
-}
 
 
 const App = () => {
